b05de149f093d06478d3bc536d31114e1fcc22c4
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-09-08 14:48:22 +0000

    ✦ I've corrected the error in src/app/login/page.tsx. Your requested changes should now be working as   expected.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-'''use client'
+'use client'
 
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
@@ -24,4 +24,4 @@ export default function LoginPage() {
     </div>
   )
 }
-'''
+
